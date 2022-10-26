533c422d9563d1bbbf0f7c59bc13b7cf7b83f47d
maudlin27 <95254039+maudlin27@users.noreply.github.com>
2022-10-26 23:26:00 +0100

    v1 Tooltips

---
+++ lua/AI/LobbyTooltips/tooltips.lua
@@ -0,0 +1,11 @@
+
+Tooltips = {
+    aitype_spawnacu = {
+        title = "<LOC SPAWNACU_0001>Spawn ACU",
+        description = "<LOC SPAWNACU_0002>Spawns an ACU for an AI that is then transferred to a human on the same team at the start of the game",
+    },
+    aitype_spawnacucheat = {
+        title = "<LOC SPAWNACU_0003>Spawn ACUX",
+        description = "<LOC SPAWNACU_0002>Spawns an ACU for an AI that is then transferred to a human on the same team at the start of the game",
+    },
+}
